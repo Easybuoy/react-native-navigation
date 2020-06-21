a2b0f855ca9204e3f7f529c6486b2e4eecada83b
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-21 13:58:03 +0100

    refactor

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-import { MainStackNavigator } from "./navigation/StackNavigator";
-import BottomTabNavigator from "./navigation/TabNavigator";
 import DrawerNavigator from "./navigation/DrawerNavigator";
 
 export default function App() {
